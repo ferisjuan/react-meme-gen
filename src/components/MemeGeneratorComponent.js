19fe476fab90068297eb8fd266554ba4574af783
ferisjuan <feris.juan@gmail.com>
2019-08-20 16:46:24 -0500

    Mem generator is working fine!

--- src/components/MemeGeneratorComponent.js
+++ src/components/MemeGeneratorComponent.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { setState } from 'expect/build/jestMatchersObject';
 
 export default class MemeGeneratorComponent extends Component {
   constructor(props) {
@@ -10,6 +9,8 @@ export default class MemeGeneratorComponent extends Component {
       randomImage: 'http://i.imgflip.com/1bij.jpg',
       allMemeImgs: []
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -21,10 +22,49 @@ export default class MemeGeneratorComponent extends Component {
       });
   }
 
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState(prevState => {
+      return { [name]: value };
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  generateRandomImg() {
+    const rndNumber = Math.floor(Math.random() * this.state.allMemeImgs.length);
+    const rndMemeImg = this.state.allMemeImgs[rndNumber].url;
+    this.setState({ randomImage: rndMemeImg });
+  }
+
   render() {
     return (
       <div className="">
-        <img src="" alt="" />
+        <form className="meme-form" onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="topText"
+            value={this.state.topText}
+            placeholder="Top Text"
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            name="bottomText"
+            value={this.state.bottomText}
+            placeholder="Bottom Text"
+            onChange={this.handleChange}
+          />
+
+          <button onClick={() => this.generateRandomImg()}>Gen!</button>
+        </form>
+        <div className="meme">
+          <img src={this.state.randomImage} alt="" />
+          <h2 className="top">{this.state.topText}</h2>
+          <h2 className="bottom">{this.state.bottomText}</h2>
+        </div>
       </div>
     );
   }
